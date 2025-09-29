20a4f28fee8dc746ff6654583797907e68dfb870
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-09-29 09:40:13 -0300

    style: finish login page style

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
-console.log('Script loaded');
+document.addEventListener('DOMContentLoaded', function () {
+
+    const clearButton = document.querySelector('.clear-button');
+    const loginForm = document.querySelector('.login-form');
+
+    if (clearButton && loginForm) {
+        clearButton.addEventListener('click', function () {
+            loginForm.reset();
+        });
+    }
+
+    const passwordInput = document.querySelector('#password-input');
+    const togglePasswordButton = document.querySelector('#toggle-password');
+
+    if (passwordInput && togglePasswordButton) {
+        togglePasswordButton.addEventListener('click', function () {
+            const isPassword = passwordInput.getAttribute('type') === 'password';
+
+            if (isPassword) {
+                passwordInput.setAttribute('type', 'text');
+                togglePasswordButton.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+            } else {
+                passwordInput.setAttribute('type', 'password');
+                togglePasswordButton.innerHTML = '<i class="fa-solid fa-eye"></i>';
+            }
+        });
+    }
+});
